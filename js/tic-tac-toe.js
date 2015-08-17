d990b8092337b3262813710caf3666df5b61b568
ALEXANDRA SWAN <alexandraswan@gmail.com>
2015-08-17 14:15:45 +1000

    add game functions

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,1 +1,59 @@
-console.log("test")
+ //  _______ _        _______           _______         
+ // |__   __(_)      |__   __|         |__   __|        
+ //    | |   _  ___     | | __ _  ___     | | ___   ___ 
+ //    | |  | |/ __|    | |/ _` |/ __|    | |/ _ \ / _ \
+ //    | |  | | (__     | | (_| | (__     | | (_) |  __/
+ //    |_|  |_|\___|    |_|\__,_|\___|    |_|\___/ \___|
+                                                     
+                                                    
+//Pseudo-code
+
+//Track player turns
+
+//display X or O on square when player clicks
+
+var Game = {
+
+		playerTurn: 'X',
+
+		play : function() {
+
+			$('.square').on('click', function(e) {
+				var player = Game.playerTurn;			
+				$('#'+e.target.id).append($('<p>'+player+'</p>'));
+				Game.switchTurn();
+			})
+
+		},
+
+		switchTurn: function() {
+			if (Game.playerTurn === 'X'){
+				Game.playerTurn = 'O';
+			} else {
+				Game.playerTurn = 'X';
+			}
+			$('.player').toggle();
+		},
+
+		newGame: function() {
+			$('.new-game').on('click', function() {
+				$('p').remove();
+			})
+		},
+
+
+		win : function () {
+
+			
+
+		}
+
+}
+
+//Uncover correct square
+
+$(document).ready(function () {
+	Game.play();
+	Game.newGame();
+
+});
