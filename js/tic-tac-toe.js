80d0f3f999815f10805a83acb63f32c3253f5fdc
ALEXANDRA SWAN <alexandraswan@gmail.com>
2015-08-17 16:49:17 +1000

    update game functions

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -4,26 +4,26 @@
  //    | |  | |/ __|    | |/ _` |/ __|    | |/ _ \ / _ \
  //    | |  | | (__     | | (_| | (__     | | (_) |  __/
  //    |_|  |_|\___|    |_|\__,_|\___|    |_|\___/ \___|
-                                                     
-                                                    
-//Pseudo-code
-
-//Track player turns
-
-//display X or O on square when player clicks
 
 var Game = {
 
 		playerTurn: 'X',
+		wins: [],
+		moves: 0,
+		playon: true,
 
 		play : function() {
-
 			$('.square').on('click', function(e) {
-				var player = Game.playerTurn;			
-				$('#'+e.target.id).append($('<p>'+player+'</p>'));
-				Game.switchTurn();
+				if (Game.playon) {
+					var player = Game.playerTurn;			
+					$('#'+e.target.id).append($('<p>'+player+'</p>')).addClass('' + player);		
+					Game.moves++;
+					Game.playon = Game.checkWin(player);
+					Game.switchTurn();
+				} else {
+					return;
+				}
 			})
-
 		},
 
 		switchTurn: function() {
@@ -37,20 +37,55 @@ var Game = {
 
 		newGame: function() {
 			$('.new-game').on('click', function() {
+				Game.playon = true;
 				$('p').remove();
+				Game.wins = [];
+				Game.moves = 0;
+				$('.square').removeClass('X');
+				$('.square').removeClass('O');
+				$('#draw').css('display','none');
+				$('#X').css('display','none');
+				$('#O').css('display','none');
 			})
 		},
 
+		checkWin : function (player) {		
+			Game.wins.push($('.col-1.'+player).length);
+			Game.wins.push($('.col-2.'+player).length);
+			Game.wins.push($('.col-3.'+player).length);
+			Game.wins.push($('#row-1').children(""+player).length);
+			Game.wins.push($('#row-2').children(""+player).length);
+			Game.wins.push($('#row-3').children(""+player).length);
+			Game.wins.push( $('#square1.'+player).add('#square5.'+player).add('#square9.'+player).length);
+			Game.wins.push( $('#square3.'+player).add('#square5.'+player).add('#square7.'+player).length);
 
-		win : function () {
-
-			
-
+			for (var i = 0; i < Game.wins.length; i++){
+				if (Game.wins[i] === 3){
+					$('#'+player).css('display','block');
+					return false;
+				}  
+			} return true;
+		},
+		checkDraw: function() {
+			if (Game.moves === 9) {
+				$('#draw').css('display','block');
+				return false;
+			} else {
+				return true;
+			}
 		}
 
 }
 
-//Uncover correct square
+
+
+// help: long version
+// $('.row').each(function (rowNumber) {
+//   var $cols = $(this).find('.square');
+//   $cols.each(function (colNumber) {
+//     $(this).html('<p>' + colNumber + ', ' + rowNumber + '</p>');
+//   });
+// });
 
 $(document).ready(function () {
 	Game.play();
