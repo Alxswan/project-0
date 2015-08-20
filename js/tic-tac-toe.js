62b12039fb519e290fd071ab39e5045d36713d92
ALEXANDRA SWAN <alexandraswan@gmail.com>
2015-08-20 12:51:55 +1000

    add comments

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -12,9 +12,10 @@ var p2 = 'O';
 var Game = {
 
 		initialise: function() {
+			//sets variables to initial play state
 			this.playerTurn = p1;	
-			this.lastPlayerWins = [];	
-			this.nextPlayerWins = [];
+			this.lastPlayerWins = []; //will hold the board layout of the previous player
+			this.nextPlayerWins = []; //will hold the board layout of the next player
 			this.moves = 0;	
 			this.playon = true;	
 			this.xWin = 0;	
@@ -29,22 +30,25 @@ var Game = {
 		},
 
 		play: function() {
+			//Adds 'X', 'O' or player inital as paragraph to board in square player has clicked
+
 			$('.square').on('click', function(e) {	
 
 				if (Game.playon && ($(this).text() === "") && $(this).children('img').length === 0) {
 					var player = Game.playerTurn;
 
-					if (Game.tay) {
-						var add = ($(Game.tayPic));
+					if (Game.tay) { 
+						var add = ($(Game.tayPic)); //p1 can play as image instead of 'X'
 
 					} else {
 						var add = ($('<p>'+player+'</p>'))
 					}
-				
+
 					$(this).append(add).addClass('' + player).hide().fadeIn();	
 					
 					Game.gameCheck(player);
 					
+					//Run AI.play plays if Game.AI has been clicked
 					if (Game.AI) {
 						AI.play();
 						Game.switchTurn();
@@ -59,6 +63,7 @@ var Game = {
 		},
 
 		gameCheck: function(player) {
+		//Checks to see if there has been a win or draw and sets Game.playon to false if so
 					Game.nextPlayerWins = Game.lastPlayerWins;
 					Game.lastPlayerWins = [];
 					Game.moves++;
@@ -69,6 +74,7 @@ var Game = {
 		},
 
 		switchTurn: function() {
+		//Changes the player turn from p1 to p2, also handles display and bey/tay modes
 			if (Game.playerTurn === p1){
 				Game.playerTurn = p2;
 			} else {
@@ -86,7 +92,10 @@ var Game = {
 		},
 
 		checkWin: function (player) {	
-			
+		//Checks whether the current player has won
+		//Looks to see how many squares a player has occupied in each 'win combination' i.e. each column, row and diagonal
+		//Pushes that value to an array
+
 			Game.lastPlayerWins.push($('.col-1.'+player).length);
 			Game.lastPlayerWins.push($('.col-2.'+player).length);
 			Game.lastPlayerWins.push($('.col-3.'+player).length);
@@ -96,6 +105,8 @@ var Game = {
 			Game.lastPlayerWins.push($('.diag-1.'+player).length);
 			Game.lastPlayerWins.push($('.diag-2.'+player).length);
 
+		//Player has won if the length of any of the array values is equals to the size (width) of the board
+		//Sets display and updates player tally counter
 			for (var i = 0; i < Game.lastPlayerWins.length; i++) {
 				if (Game.lastPlayerWins[i] === Game.boardSize) {
 					$('#'+player).css('display','block');
@@ -103,6 +114,7 @@ var Game = {
 
 					$(""+AI.wins[i]).css('background-color', 'rgba(255,254,78,0.7)');
 					
+					//Increment player's win count and update scoreboard
 					if (player === p1){
 						Game.xWin++;
 						$('#p1-total').text(""+Game.xWin);
@@ -117,6 +129,7 @@ var Game = {
 		},
 
 		checkDraw: function() {
+		//Checks if number of moves is equal to the number of squares in the board, in which case there has been a draw
 			if (Game.moves === Game.boardSize * Game.boardSize) {
 				$('#draw').css('display','block');
 				$('.player-turn').hide();
@@ -127,6 +140,8 @@ var Game = {
 		},
 
 		playfourByFour: function() {
+		//On click appends extra column to each 'row' div and clones the last row to make 4th row
+		//Changes 'diagonal' classes to correctly reflect diagonals of 4x4
 			$('.4x4').on('click', function() {
 				Game.newGame();
 				if (!Game.fourbyfour) {
@@ -151,6 +166,7 @@ var Game = {
 		},
 
 		playNormal: function() {
+		//Reverts game size and diagonals back to 3x3 grid
 			$('.normal').on('click', function() {
 				Game.boardSize = 3;
 				$('.board').removeClass('four').addClass('three');
@@ -172,6 +188,7 @@ var Game = {
 		},
 
 		playBey: function() {
+		//Sets a picture of Beyonce to a random winning combination because bow down bitches
 			$('.bey').on('click', function() {
 				$('p').remove();
 				$('img').remove();
@@ -188,6 +205,7 @@ var Game = {
 		},
 
 		playTay: function() {
+		//On click allows for p1 to play as Taylor Swift
 			$('.tay').on('click', function() {
 				Game.newGame();
 				Game.tay = true;
@@ -196,6 +214,7 @@ var Game = {
 		},
 
 		playName: function() {
+		//Prompts user for input of name and takes first initial, sets variables p1 and p2 as player initilals
 			$('.names').on('click', function() {
 				Game.newGame();
 				Game.AI = false;
@@ -206,7 +225,7 @@ var Game = {
 				while (p2 === p1) {
 					p2 = prompt("Nah, pick another name player 2! (Hint: use a different initial)").slice(0,1).toUpperCase();
 				}
-
+		//Change id associated with player for purpose of displaying winner upon win
 				$('#X').attr('id', ''+p1);
 				$('#O').attr('id', ''+p2);
 				Game.playerTurn = p1;
@@ -215,6 +234,7 @@ var Game = {
 		},
 
 		playAI: function() {
+		//Sets Game.AI to true if button clicked
 			$('.AI').on('click', function() {
 				if (!Game.fourbyfour){
 				Game.newGame();
@@ -226,6 +246,7 @@ var Game = {
 		},
 
 		playAIp1: function() {
+		//Allows AI to play as player 1 by switching turns immediately after new game initiated
 			$('.firstplay').on('click', function() {
 				if (!Game.fourbyfour){
 				Game.newGame();
@@ -240,7 +261,7 @@ var Game = {
 		},
 
 		newGame: function() {
-
+		//Remove all plays from board and set variables to allow new play
 				$('.square').removeClass(''+p1);
 				$('.square').removeClass(''+p2);
 				Game.playon = true;
@@ -267,6 +288,7 @@ var Game = {
 		},
 
 		resetGame: function() { 
+		//Resets game on button click
 			$('.new-game').on('click', function() {
 				Game.bey = false;
 				Game.tay = false;
@@ -275,6 +297,7 @@ var Game = {
 		},
 
 		resetScoreboard: function() {
+		//Resets win tally to 0
 			$('button.reset').on('click', function() {
 				Game.xWin = 0;
 				Game.oWin = 0;
@@ -288,17 +311,23 @@ var Game = {
 }
 
 var AI = {
-
+	
+	//Order of last and next 'player Wins' array (which holds how many occurrences of player in each 'win' combination in this order):
 	wins: ['.col-1','.col-2','.col-3','.row-1','.row-2','.row-3','.diag-1','.diag-2'],
 
 	play: function() {		
+	//Plays AI as 'O'
 		if (!Game.tay){		
-			if (Game.moves < 9 && $('.center').hasClass('X')) {
-				this.winMove() || this.blockMove() || this.centerPlay() || this.specialEdgePlay() || this.cornerPlay() || this.edgePlay();
 
+			//Check through each combination to see if 'true' (i.e. move possible), do that move if possible
+			//if centre is full, preference corner over edge, else preference corner
+			if (Game.moves < 9 && $('.center').hasClass('X')) {			
+				this.winMove() || this.blockMove() || this.centerPlay() || this.specialEdgePlay() || this.cornerPlay() || this.edgePlay();
+			
 			} else if (Game.moves < 9 && !$('.center').hasClass('X')){
 					this.winMove() || this.blockMove() || this.centerPlay() || this.specialEdgePlay() || this.edgePlay() || this.cornerPlay();
 			}
+		//Allows players playing as Taylor Swift to beat AI
 		} else if (Game.moves < 9 && Game.checkWin('X')){
 				   this.edgePlay() || this.cornerPlay() || this.centerPlay() || this.specialEdgePlay();
 
@@ -309,6 +338,7 @@ var AI = {
 	},
 
 	centerPlay: function() {
+	//Play in the centre if it is emply
 		if (!($('.row-2.col-2').is('.O,.X'))) {
 			$('.row-2.col-2').append($('<p>O</p>')).addClass('O').hide().fadeIn(4000);
 			return true;
@@ -316,6 +346,8 @@ var AI = {
 	},
 
 	cornerPlay: function() {
+	//Check which corners are empty, if there are 3 or more empty play in third, 
+	//if there are 2 empty play in second, if there is 1 empty play in 1
 			var corners = $('.corner').not('.O').not('.X')
 			if (corners.length === 2){
 			corners.eq(1).append($('<p>O</p>')).addClass('O').hide().fadeIn(4000);
@@ -331,6 +363,7 @@ var AI = {
 	},
 
 	edgePlay: function() {
+	//Play on the edge if there are two or more edges remaining
 			var edges = $('.edge').not('.O').not('.X')
 			if (edges.length >= 2) {
 				edges.eq(0).append($('<p>O</p>')).addClass('O').hide().fadeIn(4000);
@@ -339,6 +372,7 @@ var AI = {
 	},
 
 	specialEdgePlay: function() {
+	//Special case where in particular configuration play in adjacent edge 
 			if ($('.row-1.col-1').hasClass('X') && $('.row-3.col-2').hasClass('X') && !$('.row-2.col-1').hasClass('O') ) {
 				$('.row-2.col-1').append($('<p>O</p>')).addClass('O').hide().fadeIn(4000);
 				return true;
@@ -349,6 +383,7 @@ var AI = {
 	},
 
 	blockMove: function(){
+	//If the oposing player can win next turn, block play
 		for (var i = 0; i < Game.lastPlayerWins.length; i++){
 			if (Game.lastPlayerWins[i] === 2 && Game.nextPlayerWins[i] === 0){
 				var move = AI.wins[i];
@@ -359,6 +394,7 @@ var AI = {
 	},
 
 	winMove: function() {
+	//If there is an opportunity to win, win
 		for (var i = 0; i < Game.nextPlayerWins.length; i++){
 			if (Game.nextPlayerWins[i] === 2 && Game.lastPlayerWins[i] === 0){
 				var move = AI.wins[i];
